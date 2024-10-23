28c3c8425a67a7bf1742da1a352162138b4cd887
alexbukreev <padmasherab@icloud.com>
2024-10-23 19:53:51 +0300

    Update norma index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Инициализация React-приложения
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,25 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Пример запроса на сервер для тестирования уязвимостей
-function testServer() {
-  fetch('/../../etc/passwd') // Проверяем на Directory Traversal
-    .then(response => response.text())
-    .then(data => console.log('Server Data:', data))
-    .catch(err => console.error('Error fetching /etc/passwd:', err));
-
-  fetch('/proc/version') // Проверка версии ядра системы
-    .then(response => response.text())
-    .then(data => console.log('Kernel Version:', data))
-    .catch(err => console.error('Error fetching /proc/version:', err));
-
-  fetch('/var/log/nginx/access.log') // Попытка доступа к логам Nginx
-    .then(response => response.text())
-    .then(data => console.log('Nginx Logs:', data))
-    .catch(err => console.error('Error fetching Nginx logs:', err));
-}
-
-testServer();
-
-// Логирование метрик производительности приложения
-reportWebVitals(console.log);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
